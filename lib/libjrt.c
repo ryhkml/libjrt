45f9918ca7bc0aef5bb52aa9fceb4bc74e47b025
ryhkml <reyhank95@hotmail.com>
2025-02-03 09:41:01 +0700

    Refactor: initialize test summary

--- lib/libjrt.c
+++ lib/libjrt.c
@@ -1,6 +1,10 @@
 #include "libjrt.h"
 
-TestSummary test_summary = {0, 0, 0};
+TestSummary test_summary = {
+    .tests_run = 0,
+    .tests_failed = 0,
+    .tests_passed = 0,
+};
 
 int current_test_failed = 0;
 int counter_assert_run = 0;
